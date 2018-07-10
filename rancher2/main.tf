8e1d503e0c737a2bcfeeabcdc10104abbd01562a
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2018-07-10 14:43:37 -0400

    [rancher2] clean up hardcoded vars

--- rancher2/main.tf
+++ rancher2/main.tf
@@ -6,18 +6,8 @@ provider "template" {
   version = "~> 0.1"
 }
 
-resource "openstack_images_image_v2" "rancheros" {
-  name = "RancherOS 1.3.0"
-  image_source_url = "https://github.com/rancher/os/releases/download/v1.3.0/rancheros-openstack.img"
-  container_format = "bare"
-  disk_format = "qcow2"
-  min_disk_gb = "1"
-  min_ram_mb = "512"
-  protected = "false"
-}
-
 data "openstack_compute_keypair_v2" "kp" {
-  name = "candig_dev_key"
+  name = "${var.key_pair}"
 }
 
 data "template_file" "rancher_user_data" {
@@ -30,7 +20,7 @@ data "template_file" "rancher_user_data" {
 
 resource "openstack_compute_instance_v2" "rancher" {
   name = "rancher"
-  image_id = "${openstack_images_image_v2.rancheros.id}"
+  image_name = "${var.image_name}"
   flavor_name = "m1.medium"
 
   user_data = "${data.template_file.rancher_user_data.rendered}"
@@ -38,13 +28,13 @@ resource "openstack_compute_instance_v2" "rancher" {
   security_groups = [
     "default",
     "ssh_from_kidnet",
-    "DEBUG_ALL"
+    "debug_all"
   ]
 
-  config_drive = "true"
+  config_drive = true
 
   network {
-    name = "CanDIG_Lan1"
+    name = "${var.network}"
   }
 }
 
@@ -52,7 +42,7 @@ resource "openstack_networking_floatingip_v2" "rancher" {
   pool = "${var.floating_ip_pool}"
 }
 
-resource "openstack_compute_floatingip_associate_v2" "myip" {
+resource "openstack_compute_floatingip_associate_v2" "rancher" {
   floating_ip = "${openstack_networking_floatingip_v2.rancher.address}"
   instance_id = "${openstack_compute_instance_v2.rancher.id}"
   fixed_ip    = "${openstack_compute_instance_v2.rancher.network.0.fixed_ip_v4}"
